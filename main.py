e53cca534d94b6395d271ef8801554464fa5b840
betrand2470 <70488885+betrand2470@users.noreply.github.com>
2023-02-21 18:12:09 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "World from General Assembly")
+    name = os.environ.get("NAME", "World from General Assembly!!")
     return f"Hello {name}!!!"
 
 
